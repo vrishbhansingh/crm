f7e76d0904bfbedb3e12576f9b8664eb86dc8a11
Vrishbhan <vrishbhansingh321@gmail.com>
2026-02-21 23:36:51 +0530

    spacing

--- resources/views/user/closedList/closed-lead.blade.php
+++ resources/views/user/closedList/closed-lead.blade.php
@@ -15,6 +15,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/toastr.js/latest/toastr.min.css" />
 
     <style>
+        
         /* HEADER */
         .crm-header {
             display: flex;
